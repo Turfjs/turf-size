fb5d25a0c83a8c45f94749618bcd6e16a2aeaa7d
Morgan Herlocker <morgan.herlocker@gmail.com>
2014-04-13 12:49:50 -0400

    working properly with factors

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 module.exports = function(bbox, factor){
-  var xDistance = ((bbox[2] - bbox[0]) / 2) * factor
-  var yDistance = ((bbox[3] - bbox[1]) / 2) * factor
+  var currentXDistance = (bbox[2] - bbox[0]) 
+  var currentYDistance = (bbox[3] - bbox[1])
+  var newXDistance = currentXDistance * factor
+  var newYDistance = currentYDistance * factor
+  var xChange = newXDistance - currentXDistance
+  var yChange = newYDistance - currentYDistance
+  console.log(currentXDistance)
+  console.log(newXDistance)
+  console.log(xChange)
+  //var xDistance = (((bbox[2] - bbox[0]) / 2) * factor) /2
+  //var yDistance = (((bbox[3] - bbox[1]) / 2) * factor) /2
 
-  var lowX = bbox[0] - xDistance
-  var lowY = bbox[1] - yDistance
-  var highX = xDistance + bbox[2]
-  var highY = yDistance + bbox[3]
+  var lowX = bbox[0] - (xChange / 2)
+  var lowY = bbox[1] - (yChange / 2)
+  var highX = (xChange / 2) + bbox[2]
+  var highY = (yChange / 2) + bbox[3]
 
   var sized = [lowX, lowY, highX, highY]
   return sized
