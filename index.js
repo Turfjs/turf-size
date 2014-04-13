5b7eff4a8c594ff5453883513a6ee9dd138106f5
Morgan Herlocker <morgan.herlocker@gmail.com>
2014-04-13 12:50:14 -0400

    clean up logging and dead code

--- index.js
+++ index.js
@@ -5,11 +5,6 @@ module.exports = function(bbox, factor){
   var newYDistance = currentYDistance * factor
   var xChange = newXDistance - currentXDistance
   var yChange = newYDistance - currentYDistance
-  console.log(currentXDistance)
-  console.log(newXDistance)
-  console.log(xChange)
-  //var xDistance = (((bbox[2] - bbox[0]) / 2) * factor) /2
-  //var yDistance = (((bbox[3] - bbox[1]) / 2) * factor) /2
 
   var lowX = bbox[0] - (xChange / 2)
   var lowY = bbox[1] - (yChange / 2)
